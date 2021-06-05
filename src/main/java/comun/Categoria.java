3f269559f71915814a1e647872ac65d810eea865
jmpgr <jmpgr1@gmail.com>
2021-06-05 22:28:18 +0200

    cat y ususario

---
+++ src/main/java/comun/Categoria.java
@@ -0,0 +1,21 @@
+package comun;
+
+public class Categoria {
+	private String nombreCategoria;
+	
+	
+
+	public Categoria() {
+		super();
+	}
+
+	public String getNombreCategoria() {
+		return nombreCategoria;
+	}
+
+	public void setNombreCategoria(String nombreCategoria) {
+		this.nombreCategoria = nombreCategoria;
+	}
+
+	
+}
---
+++ src/main/java/comun/Usuario.java
@@ -0,0 +1,22 @@
+package comun;
+
+public class Usuario {
+	private String nombre;
+	
+	
+
+	public Usuario() {
+		super();
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+
+}
