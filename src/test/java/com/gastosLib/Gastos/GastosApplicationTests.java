972ccb17a2c57663293a4556224cc1391f9162ad
jmpgr <jmpgr1@gmail.com>
2021-06-05 19:20:21 +0200

    subir Gastos

---
+++ src/test/java/com/gastosLib/Gastos/GastosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gastosLib.Gastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
